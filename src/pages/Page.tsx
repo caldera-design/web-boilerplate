a949b12d03d1e08f6596f7bbb9f591871c42702e
Jon Brennecke <jpbrennecke@gmail.com>
2017-11-25 22:36:29 -0800

    cmmitting last work

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,19 +1,77 @@
 
 import React, { Component } from 'react';
+import { autobind } from 'core-decorators';
+
+import WebGLCanvas from 'webgl-canvas';
+import WebGLText from 'webgl-fancy-text';
+import provideDimensions from 'provide-dimensions';
+
+type Dimensions = {
+    width: number;
+    height: number;
+};
 
 type AppProps = {
     children?: undefined;
+    dimensions: Dimensions;
 };
 
-export default class Page extends Component<AppProps, {}> {
+type AppState = {
+    mouse: {
+        x: number;
+        y: number;
+    };
+};
+
+@provideDimensions
+@autobind
+export default class Page extends Component<AppProps, AppState> {
 
     public props: AppProps;
 
+    private text: WebGLText;
+
+    public state: AppState = {
+        mouse: { x: 0, y: 0 }
+    };
+
+    private onSceneInitialized(gl: WebGLRenderingContext) {
+        this.text = new WebGLText(gl);
+        this.text.setText({
+            text: 'Caldera',
+            fontFamily: 'Quicksand, sans-serif',
+            fontSize: 100,
+            textAlign: 'center'
+        });
+    }
+
+    private onSceneRender(gl: WebGLRenderingContext) {
+        const { dimensions } = this.props;
+        const { mouse } = this.state;
+        this.text.render({
+            mouse,
+            rotation: 0,
+            position: { x: dimensions.width / 2, y: dimensions.height / 2 - 50 },
+            center: { x: 0, y: 0 },
+            resolution: dimensions,
+            size: { width: 300, height: 100 }
+        });
+    }
+
     public render() {
+        const { width, height } = this.props.dimensions;
         return (
-            <div>
-                <h1>Hello World!</h1>
-            </div>
+            <WebGLCanvas dimensions={{ width, height }}
+                         onSceneInitialized={this.onSceneInitialized}
+                         onSceneRender={this.onSceneRender}
+                         onMouseMove={e => {
+                             this.setState({
+                                 mouse: {
+                                     x: e.nativeEvent.offsetX,
+                                     y: e.nativeEvent.offsetY
+                                 }
+                             });
+                         }}/>
         );
     }
 }
